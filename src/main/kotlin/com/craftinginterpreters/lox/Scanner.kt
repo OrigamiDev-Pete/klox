ff05eceafb2449654ed078efec9f6bacefb65edc
Peter DV <pdevroom@outlook.com>
2022-06-13 15:24:05 +1000

    Initial commit

---
+++ src/main/kotlin/com/craftinginterpreters/lox/Scanner.kt
@@ -0,0 +1,173 @@
+package com.craftinginterpreters.lox
+import com.craftinginterpreters.lox.TokenType.*
+
+private val keywords: Map<String, TokenType> = mapOf(
+    "and" to AND,
+    "class" to CLASS,
+    "else" to ELSE,
+    "false" to FALSE,
+    "for" to FOR,
+    "fun" to FUN,
+    "if" to IF,
+    "nil" to NIL,
+    "or" to OR,
+    "print" to PRINT,
+    "return" to RETURN,
+    "super" to SUPER,
+    "this" to THIS,
+    "true" to TRUE,
+    "var" to VAR,
+    "while" to WHILE
+)
+
+class Scanner(private val source: String) {
+    private val tokens = ArrayList<Token>()
+
+    private var start = 0
+    private var current = 0
+    private var line = 1
+
+    fun scanTokens(): List<Token> {
+        while (!isAtEnd()) {
+            start = current
+            scanToken()
+        }
+
+        tokens.add(Token(EOF, "", null, line))
+        return tokens
+    }
+
+    private fun isAtEnd() = current >= source.length
+
+    private fun scanToken() {
+        when (val c = advance()) {
+            '(' -> addToken(LEFT_PAREN)
+            ')' -> addToken(RIGHT_PAREN)
+            '{' -> addToken(LEFT_BRACE)
+            '}' -> addToken(RIGHT_BRACE)
+            ',' -> addToken(COMMA)
+            '.' -> addToken(DOT)
+            '-' -> addToken(MINUS)
+            '+' -> addToken(PLUS)
+            ';' -> addToken(SEMICOLON)
+            '*' -> addToken(STAR)
+            '!' -> addToken(if (match('=')) BANG_EQUAL else BANG)
+            '=' -> addToken(if (match('=')) EQUAL_EQUAL else EQUAL)
+            '<' -> addToken(if (match('=')) LESS_EQUAL else LESS)
+            '>' -> addToken(if (match('=')) GREAT_EQUAL else GREATER)
+
+            '/' -> if (match('/')) {
+                while (peek() != '\n' && !isAtEnd()) advance()
+            } else if (match('*')) {
+                multilineComment()
+            } else {
+                addToken(SLASH)
+            }
+
+            // Ignore whitespace.
+            ' ', '\r', '\t' -> {}
+
+            '\n' -> line++
+
+            '"' -> string()
+
+            else -> {
+                if (isDigit(c)) {
+                    number()
+                } else if (isAlpha(c)) {
+                    identifier()
+                } else {
+                    error(line, "Unexpected character.")
+                }
+            }
+        }
+    }
+
+    private fun identifier() {
+        while (isAlphaNumeric(peek())) advance()
+        val text = source.substring(start, current)
+        var type = keywords[text]
+        if (type == null) type = IDENTIFIER
+        addToken(type)
+    }
+
+    private fun number() {
+        while(isDigit(peek())) advance()
+
+        // Look for fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            //Consume the "."
+            advance()
+            while (isDigit(peek())) advance()
+        }
+        addToken(NUMBER, source.substring(start, current).toDouble())
+    }
+
+    private fun string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++
+            advance()
+        }
+
+        if (isAtEnd()) {
+            error(line, "Unterminated string.")
+            return
+        }
+
+        // The closing ".
+        advance()
+
+        // Trim the surrounding quotes.
+        val value = source.substring(start + 1, current - 1)
+        addToken(STRING, value)
+    }
+
+    private fun multilineComment() {
+        while (peek() != '*' && peekNext() != '/' && !isAtEnd()) {
+            if (peek() == '\n') line++
+            advance()
+        }
+
+        if (isAtEnd()) {
+            error(line, "Unterminated multi-line comment.")
+            return
+        }
+
+        // Consume the "*/" characters.
+        advance()
+        advance()
+    }
+
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd()) return false
+        if (source[current] != expected) return false
+
+        current++
+        return true
+    }
+
+    private fun peek(): Char {
+        if (isAtEnd()) return '\u0000'
+        return source[current]
+    }
+
+    private fun peekNext(): Char {
+        if (current + 1 >= source.length) return '\u0000'
+        return source[current+1]
+    }
+
+    private fun isAlpha(c: Char) = (c in 'a'..'z') || (c in 'A'..'Z') || c == '_'
+
+    private fun isAlphaNumeric(c: Char) = isAlpha(c) || isDigit(c)
+
+    private fun isDigit(c: Char) = c in '0'..'9'
+
+    private fun advance(): Char = source[current++]
+
+    private fun addToken(type: TokenType) = addToken(type, null)
+
+    private fun addToken(type: TokenType, literal: Any?) {
+        val text = source.substring(start, current)
+        tokens.add(Token(type, text, literal, line))
+    }
+}
